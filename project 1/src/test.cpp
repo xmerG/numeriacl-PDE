3a6a44cbd30c6bc30bbe4d06af7831bdd9113aea
GMSSSM <2649713748@qq.com>
2025-03-04 14:04:10 +0800

    add a test example

--- project 1/src/test.cpp
+++ project 1/src/test.cpp
@@ -3,6 +3,7 @@
 #include<cmath>
 using namespace std;
 
+const double b=cos(1.0);
 const double a=sin(1.0);
 class Laplacian:public Function{
     double operator()(double x, double y) const{
@@ -33,8 +34,22 @@ class DirichletF:public Function{
 
 class NeumannF:public Function{
     double operator()(double x, double y) const{
-        return 0.0;
-        //???????????????????如何对正方形区域定义外法向量？？？？？？？？？？？？？？？？？？
+        if(x==0.0){
+            return -exp(y);
+        }
+        else if(x==1.0){
+            return b*exp(y+a);
+        }
+        else if(y==0.0){
+            return -exp(sin(x));
+        }
+        else if(y==1.0){
+            return exp(sin(x)+1);
+        }
+        else {
+            cerr<<"not defined"<<endl;
+            return -1;
+        }
     }
 };
 
@@ -46,14 +61,16 @@ class primitive:public Function{
 
 int main(){
     Laplacian f1;
-    DirichletF g;
+    //DirichletF g;
     primitive f0;
-    EquationSolver<Domain::regular, BoundaryCondition::Dirichlet> solver1(4, f1);
-    solver1.solveEquation(g);
-    solver1.norm_error(f0);
-    solver1.print("test.json", f0);
+    //EquationSolver<Domain::regular, BoundaryCondition::Dirichlet> solver1(4, f1);
+    //solver1.solveEquation(g);
+    //solver1.norm_error(f0);
+    //solver1.print("test.json", f0);
 
-    //EquationSolver<Domain::regular, BoundaryCondition::Neumann> solver2(4,f1);
-    //solver2.solveEquation(g);
+    NeumannF g1;
+    EquationSolver<Domain::regular, BoundaryCondition::Neumann> solver2(4,f1);
+    solver2.solveEquation(g1);
+    solver2.print("test.json", f0);
     return 0;
 }
