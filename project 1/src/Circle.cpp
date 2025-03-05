410c1a087ec68a71ca1446ce0265560fb0a4539b
GMSSSM <2649713748@qq.com>
2025-03-05 19:19:27 +0800

    modify class circle

--- project 1/src/Circle.cpp
+++ project 1/src/Circle.cpp
@@ -17,3 +17,23 @@ bool Circle::inCircle(double x, double y) const{
 double Circle::distance(double x, double y) const{
     return sqrt(pow(x0-x, 2) + pow(y0-y, 2));
 }
+
+double Circle::x_distance_to_circle(double x, double y) const{
+    if(this->inCircle(x,y)){
+        cerr<<"can't calculate cause the point is in circle"<<endl;
+        return -1;
+    }
+    else{
+        return abs(x-x0)-sqrt(pow(radius, 2)-pow((y-y0), 2));
+    }
+}
+
+double Circle::y_distance_to_circle(double x, double y) const{
+    if(this->inCircle(x,y)){
+        cerr<<"can't calculate cause the point is in circle"<<endl;
+        return -1;
+    }
+    else{
+        return abs(y-y0)-sqrt(pow(radius, w)-pow((x-x0), 2));
+    }
+}
