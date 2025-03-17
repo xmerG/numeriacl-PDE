64725ad5b4ec879f9227dbdc66faa42a20da58af
GMSSSM <2649713748@qq.com>
2025-03-17 20:44:25 +0800

    add makefile and modify plotA.py

--- project 1/draw/plotA.py
+++ project 1/draw/plotA.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os  
+import os
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import griddata
 
@@ -13,40 +13,49 @@ boundaryCondition = {
     2: 'Mixed'
 }
 
-domain={
-    0:'regular',
-    1:'irregular'
+domain = {
+    0: 'regular',
+    1: 'irregular'
 }
 
-path=os.path.join('..', 'output','test.json')
+path = os.path.join('output', 'output.json')
 with open(path, 'r') as file:
-    data=json.load(file)
+    data = json.load(file)
 
-def f(x,y):
-    return np.exp(y+np.sin(x))
 
-for i,j in enumerate(data):
-    grids=np.array(j["grids"])
-    values=np.array(j["values_on_grids"])
-    realValues=np.array(j["real_values"])
-    x=grids[:,0]
-    y=grids[:,1]
-    bc=boundaryCondition.get(j["boundary_condition"])
-    d=domain.get(j["Domain"])
-    z_values=values
-    z_real_values=realValues
-    fig=plt.figure(figsize=(10,8))
-    ax=fig.add_subplot(111,projection='3d')
-    ax.scatter(x,y,z_real_values,color='red', label='real values')
-    tri1=ax.plot_trisurf(x,y,z_real_values, cmap='viridis', alpha=0.7, label='real values')
-    ax.scatter(x,y,z_values, color='blue', label='numerical values')
-    tri2=ax.plot_trisurf(x,y,z_values, cmap='viridis', alpha=0.7, label='numerical values')
-    ax.set_title(f"domain:{d}, bc:{bc}, n={2**(3+i)}")
-    ax.set_xlabel("grids x")
-    ax.set_ylabel("grids y")
-    ax.set_zlabel("values")
-    ax.legend()
-    save=os.path.join('..', 'figure',f"f_{bc}_{d}_{i+1}.png")
+for i, j in enumerate(data):
+    grids = np.array(j["grids"])
+    values = np.array(j["values_on_grids"])
+    realValues = np.array(j["real_values"])
+    x = grids[:, 0]
+    y = grids[:, 1]
+    bc = boundaryCondition.get(j["boundary_condition"])
+    d = domain.get(j["Domain"])
+    z_values = values
+    z_real_values = realValues
+
+    fig = plt.figure(figsize=(20, 8))  
+    ax1 = fig.add_subplot(121, projection='3d')  
+    ax2 = fig.add_subplot(122, projection='3d')  
+
+
+    ax1.plot_trisurf(x, y, z_real_values, cmap='Blues', alpha=0.7)
+    ax1.scatter(x, y, z_real_values, color='red', label='real values')
+    ax1.set_title(f"Real Values (domain:{d}, bc:{bc}, n={2**(3 + i % 4)}")
+    ax1.set_xlabel("grids x")
+    ax1.set_ylabel("grids y")
+    ax1.set_zlabel("values")
+    ax1.legend()
+
+    ax2.plot_trisurf(x, y, z_values, cmap='YlOrBr', alpha=0.7)
+    ax2.scatter(x, y, z_values, color='blue', label='numerical values')
+    ax2.set_title(f"Numerical Values (domain:{d}, bc:{bc}, n={2**(3 + i % 4)}")
+    ax2.set_xlabel("grids x")
+    ax2.set_ylabel("grids y")
+    ax2.set_zlabel("values")
+    ax2.legend()
+
+    save = os.path.join('figure', f"functype_{1 + i // 24}_{bc}_{d}_{i + 1}.png")
     plt.savefig(save)
     plt.close()
-
+    print(f"figure saved: {save}")
--- project 1/src/test.cpp
+++ project 1/src/test.cpp
@@ -127,7 +127,7 @@ int main(){
             EquationSolver<Domain::irregular, BoundaryCondition::Dirichlet> solver(grid_number, *f1, c);
             solver.solveEquation(*f0);
             solver.norm_error(*f0,"../output/error.json");
-            solver.print("..output/output.json", *f0);
+            solver.print("../output/output.json", *f0);
         } else if (d == Domain::irregular && bc == BoundaryCondition::Neumann) {
             EquationSolver<Domain::irregular, BoundaryCondition::Neumann> solver(grid_number, *f1, c);
             solver.solveEquation(*func, (*f0)(1.0/grid_number, 1.0/grid_number));
@@ -144,49 +144,3 @@ int main(){
 
     return 0;
 }
-    LaplacianC f1;
-    PrimitiveC f0;
-
-    /*EquationSolver<Domain::regular, BoundaryCondition::Dirichlet> solver1(32, f1);
-    solver1.solveEquation(g);
-    solver1.norm_error(f0);
-    solver1.print("test.json", f0);*/
-
-    /*vector<int> neumann=vector<int>{1,1,1,1};
-    NeumannF g1;
-    EquationSolver<Domain::regular, BoundaryCondition::Neumann> solver2(64,f1);
-    solver2.solveEquation(g1,f0(1.0/64.0, 1.0/64.0),neumann);
-    solver2.norm_error(f0,"1.json");
-    solver2.print("test.json", f0);*/
-
-    /*vector<int> mixed=vector<int>{1,0,0,1};
-    Mixed h1;
-    EquationSolver<Domain::regular, BoundaryCondition::Mixed> solver5(32, f1);
-    solver5.solveEquation(h1, 0.0,mixed);
-    solver5.norm_error(f0);
-    solver5.print("test.json", f0);*/
-
-
-
-
-    //Circle *c = new Circle(0.5, 0.5, 0.2);
-    /*EquationSolver<Domain::irregular,BoundaryCondition::Dirichlet> solver3(8,f1,c);
-    solver3.solveEquation(f0);
-    solver3.norm_error(f0,"1.json");
-    solver3.print("test.json", f0);
-    delete c;*/
-
-    /*irreMixed m1(c,vector<int>{0,0,1,1,1});
-    EquationSolver<Domain::irregular, BoundaryCondition::Mixed> solver6(8, f1, c);
-    solver6.solveEquation(m1,0.0, vector<int>{0,0,1,1,1});
-    solver6.norm_error(f0);
-    solver6.print("../output/test1.json", f0);*/
-
-    /*Circle *c = new Circle(0.52, 0.45, 0.17);
-    NeumannC g1(c);
-    EquationSolver<Domain::irregular, BoundaryCondition::Neumann> solver4(8,f1,c);
-    solver4.solveEquation(g1, f0(1.0/8.0,1.0/8.0));
-    solver4.print("test.json", f0);
-    solver4.norm_error(f0,"1.json");
-    return 0;
-}*/
